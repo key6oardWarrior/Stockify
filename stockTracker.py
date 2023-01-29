3daeedd73de59f986f28178e87e9c9fe6a4ebb1d
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2023-01-29 12:57:44 -0500

    Update stockTracker.py

--- stockTracker.py
+++ stockTracker.py
@@ -1,6 +1,6 @@
-from Transaction_Innerface import Transaction_Innerface, Types
+from Transaction_Innerface import ITransaction, Types
 
-breq = Transaction_Innerface("https://www.capitoltrades.com/trades?txType=buy&txDate=30d&page=", Types.Buy)
+breq = ITransaction("https://www.capitoltrades.com/trades?txType=buy&txDate=30d&page=", Types.Buy)
 
 for ii in range(breq.pages):
 	breq.orgnizeData(breq.tagSearch(ii, "div", "q-table-wrapper").find_all("span"))
