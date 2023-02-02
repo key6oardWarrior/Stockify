60321d5d33e00a1b9f371c4ab5763a4d133a0fdb
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2023-02-02 18:24:22 -0500

    Init var first

--- Transaction_Innerface.py
+++ Transaction_Innerface.py
@@ -5,8 +5,8 @@ class ITransaction(Request):
 	__TYPE: Types
 
 	def __init__(self, SITE: str, TYPE: Types) -> None:
-		super().__init__(SITE)
 		self.__TYPE = TYPE
+		super().__init__(SITE)
 
 	@property
 	def transcationType(self) -> Types:
