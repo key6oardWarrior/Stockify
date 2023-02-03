b01e6e7c8b8187f2dc4337d982d9fc3cb79e4791
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2023-02-03 12:52:32 -0500

    get all stock trades that happened less than 30 days ago

--- Request.py
+++ Request.py
@@ -7,6 +7,8 @@ class Request:
 	# key = date, value = loaction
 	__house_dbLocations: dict[str: str] = dict({})
 	__senate_dbLocations: dict[str: str] = dict({})
+	__housePastDates: list = []
+	__senatePastDates: list = []
 	__senate_db = "https://senate-stock-watcher-data.s3-us-west-2.amazonaws.com/"
 	__house_db = "https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/"
 
@@ -23,9 +25,34 @@ class Request:
 			END: int = itr.string.rfind(".json")
 
 			if isHouse:
-				self.__house_dbLocations[itr.string[START: END].replace("_", "-")] = itr.string
+				self.__house_dbLocations[itr.string[START: END].replace("_", "/")] = itr.string
 			else:
-				self.__senate_dbLocations[itr.string[START: END].replace("_", "-")] = itr.string
+				self.__senate_dbLocations[itr.string[START: END].replace("_", "/")] = itr.string
+
+	def __findDates(self, dates: list, isHouse: bool=True) -> None:
+		'''
+		Get all dates that are <= __pastDates ago
+
+		# Params:
+		dates - List of dates\n
+		isHouse - Are dates for house or senate
+		'''
+		from datetime import date
+		from dateutil import relativedelta
+		from datetime import datetime
+
+		TODAY_DATE: str = date.today().isoformat().replace("-", "/")
+		TODAY_DATE: datetime = datetime.strptime(TODAY_DATE, "%Y/%m/%d")
+
+		for _date in dates:
+			PAST_DATE: datetime = datetime.strptime(_date, "%m/%d/%Y")
+			diff = relativedelta.relativedelta(TODAY_DATE, PAST_DATE)
+
+			if((diff.months < 1) and (diff.years == 0)):
+				if isHouse:
+					self.__housePastDates.append(_date)
+				else:
+					self.__senatePastDates.append(_date)
 
 	def __init__(self) -> None:
 		# trades data bases
@@ -39,9 +66,8 @@ class Request:
 		self.__set_dbLocation(houseSoup)
 		self.__set_dbLocation(senateSoup, False)
 
-		# determin if date is within 30 days of current date
-		from datetime import date
-		TODAY_DATE: str = date.today().isoformat()
+		self.__findDates(self.__house_dbLocations.keys())
+		self.__findDates(self.__senate_dbLocations.keys(), False)
 
 if __name__ == "__main__":
 	req = Request()
