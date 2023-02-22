0f2e3fe184337b0fd11e7d2d0d43495b695b80f2
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2023-02-22 03:54:42 -0500

    docs on how to compile all code

--- compile.py
+++ compile.py
@@ -1,14 +1,16 @@
 from os import walk, system
 from os.path import join
 
+# compile main dir
 dirTree: tuple = next(walk("."))[1]
 system("py -m compileall -b")
 
+# compile sub dirs
 for itr in dirTree:
 	system(f"cd {itr} && py -m compileall -b")
 
 # compile unit test
-dirTree = [join("UnitTest", "DataBase"), join("UnitTest", "Helper"),
+dirTree: list = [join("UnitTest", "DataBase"), join("UnitTest", "Helper"),
 	join("UnitTest", "Login")]
 
 for itr in dirTree:
