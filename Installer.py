f20e9a8a6880d7e397f1f38f1fef46a569b6a57c
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2023-02-27 09:20:01 -0500

    Fixed file path errors

--- Installer.py
+++ Installer.py
@@ -1,12 +1,12 @@
 from os import mkdir, system
-from shutil import move
+from shutil import copytree, move
 from sys import platform
-from shutil import copytree
 
+'''
 system(f"python -m pip install --upgrade pip")
 for itr in open("Dependencies/requirements.txt", "r").readlines():
 	system(f"python -m pip install {itr}")
-
+'''
 if platform == "win32":
 	from os.path import expanduser
 
@@ -16,9 +16,8 @@ if platform == "win32":
 	mkdir(dataDir)
 
 	# create a needed missing directory
-	pyPackages = usr + "AppData\\Local\\Programs\\Python\\Python311\\Lib\\site-packages\\"
-	mkdir(pyPackages + "src")
-	copytree(pyPackages + "lxml", pyPackages + "src")
+	pyPackages = usr + "\\AppData\\Local\\Programs\\Python\\Python311\\Lib\\site-packages\\"
+	copytree(pyPackages + "lxml", pyPackages + "src\\lxml")
 elif((platform == "linux") or (platform == "linux2")):
 	# create Stockify dir
 	dataDir = "/usr/local/Stockify"
