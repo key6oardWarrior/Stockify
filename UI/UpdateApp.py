24265c96c0f55a932b90de07c1af788ec1cdf9fb
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2023-04-16 15:16:33 -0400

    check if app has an update

---
+++ UI/UpdateApp.py
@@ -0,0 +1,49 @@
+from Helper.helper import Version, exitApp, exit
+from Helper.creds import winName
+from requests import get
+from wget import download
+from PyGUI import Text, Button, Window, FolderBrowse
+from os.path import isdir
+
+def updateScreen() -> bool:
+	layout = []
+	responce = get("https://github.com/key6oardWarrior/Stockify/blob/main/version.txt")
+	version = Version()
+
+	if responce.status_code == 200:
+		if version.version == responce.text:
+			layout.append([Text("You have the most up to date version of this software :)")])
+		else:
+			layout.append([Text("There is a new version of this software. Where would you like to download it:")])
+			layout.append([FolderBrowse(key="browse"), Button("Submit"), Button("Back")])
+
+	SIZE = len(layout)
+
+	while True:
+		if len(layout) > SIZE:
+			layout = layout[:-1]
+
+		update = Window(winName, layout)
+		event, values = update.read()
+
+		if exitApp(event, update):
+			exit(0)
+
+		if event == "Back":
+			return True
+
+		if event == "Submit":
+			if isdir(values["browse"]):
+				try:
+					download("https://github.com/key6oardWarrior/Stockify/" \
+						f"archive/refs/tags/{version.version}.zip",
+						values["browse"]
+					)
+				except:
+					layout.append([Text("Check you internet connection", text_color="red")])
+				else:
+					win = Window(winName, [[Text("To update this app close this app and run the downloaded app"), Button("Ok")]])
+					win.read()
+					win.close()
+			else:
+				layout.append([Text("The folder location selected is not a folder", text_color="red")])
