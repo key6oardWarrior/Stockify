eaab0c3ca4c8fd84b1f95596e7f41fe934cd0c0a
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2023-04-29 00:52:41 -0400

    Update compile.py

--- compile.py
+++ compile.py
@@ -9,17 +9,17 @@ dirTree: tuple = next(walk("."))
 testTree: list = [join("UnitTest", "DataBase"), join("UnitTest", "Helper"),
 	join("UnitTest", "Login")]
 
-if isdir("App") == False:
-	mkdir("App")
+if isdir("bins") == False:
+	mkdir("bins")
 
 # compile sub dirs
 for itr in dirTree[1]:
 	system(f"cd {itr} && py -m compileall -b")
 
-	# copy all compiled (non-unit test) files into App folder
-	if((itr != ".git") and (itr != "App") and (itr != ".vscode") and
+	# copy all compiled (non-unit test) files into bins folder
+	if((itr != ".git") and (itr != "bins") and (itr != ".vscode") and
 		(itr != "UnitTest")):
-		dirName = join("App", itr)
+		dirName = join("bins", itr)
 
 		if isdir(dirName) == False:
 			mkdir(dirName)
@@ -28,11 +28,11 @@ for itr in dirTree[1]:
 			if file[-4:] == ".pyc":
 				copy2(join(itr, file), dirName)
 
-# copy main compiled files to App folder
+# copy main compiled files to bins folder
 for itr in dirTree[2]:
 	if itr[-4:] == ".pyc":
 		if((itr != "compile.pyc") and (itr != "setup.pyc")):
-			copy2(itr, "App")
+			copy2(itr, "bins")
 
 # compile unit test
 for itr in testTree:
@@ -40,11 +40,11 @@ for itr in testTree:
 
 # convert the main .pyc file to a .pyw
 PATH = join("UI", "Stockify.py")
-FINAL_PATH = join("App", PATH + "w")
+FINAL_PATH = join("bins", PATH + "w")
 
 if exists(FINAL_PATH):
 	remove(FINAL_PATH)
 
-copy2(join("Dependencies", "requirements.txt"), join("App", "Dependencies"))
-rename(join("App", PATH + "c"), FINAL_PATH)
+copy2(join("Dependencies", "requirements.txt"), join("bins", "Dependencies"))
+rename(join("bins", PATH + "c"), FINAL_PATH)
 move(FINAL_PATH, "Stockify.pyw")
