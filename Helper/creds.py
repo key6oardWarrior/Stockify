141a3125807157345463687bdf93c9143c7852ae
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2023-06-25 10:50:41 -0400

    added creds

--- Helper/creds.py
+++ Helper/creds.py
@@ -1,5 +1,5 @@
 winName: str = "Stockify v1.0.0"
 # each developer needs to fill this info out
-apiLoginId: str
-transactionKey: str
-connectionString: str
+apiLoginId: str = ""
+transactionKey: str = "" 
+connectionString: str = "" 
